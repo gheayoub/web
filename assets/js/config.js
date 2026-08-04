cd1177296b572c7e1638f459a78917708ffa3a31
gheayoub <144359149+gheayoub@users.noreply.github.com>
2026-08-04 10:26:09 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,3 @@
 window.VMMO_CONFIG = {
-  // Ganti dengan URL HTTPS API Windows Anda sebelum deploy produksi.
-  // Untuk tes lokal di komputer server, gunakan: http://127.0.0.1:8765
-  API_BASE_URL: "http://127.0.0.1:8765",
-  APP_NAME: "VMmo",
-  SUPPORT_EMAIL: "support@example.com"
+  API_BASE_URL: "https://life-strenuous-annually.ngrok-free.dev"
 };
